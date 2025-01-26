51e4db58c80295ab4d6b4aeb56f051e8880f74a3
Júlio Cézar <jczrpereira77@gmail.com>
2025-01-26 13:44:50 -0300

    feat(tabs): remove logout button and related functionality from Home screen

--- app/(app)/(tabs)/index.tsx
+++ app/(app)/(tabs)/index.tsx
@@ -1,22 +1,12 @@
-import { Button } from '@/components/Button'
 import { ScreenWrapper } from '@/components/ScreenWrapper'
 import { Typo } from '@/components/Typo'
-import { auth } from '@/config/firebase'
-import { colors } from '@/constants/theme'
-import { signOut } from 'firebase/auth'
 import { StyleSheet } from 'react-native'
 
 export default function HomeScreen() {
-  const handleLoggout = async () => {
-    await signOut(auth)
-  }
-
   return (
     <ScreenWrapper>
       <Typo>Home</Typo>
-      <Button onPress={handleLoggout}>
-        <Typo color={colors.black}>Loggout</Typo>
-      </Button>
+
     </ScreenWrapper>
   )
 }
